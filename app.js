ac22270fd8faa6495d491953d0a6554125efd0fc
atharvabhanage02 <atharvabhanage2002@gmail.com>
2021-10-11 09:32:24 +0530

    styles

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ function calculateProfitOrLoss(initial,qty,current){
     if(initial>current){
         var loss = (initial-current)*qty;
         var lossPercent = (loss/(initial*qty))*100;
-        output.innerText = `You are in loss ${loss} with ${lossPercent}%`; 
+        output.innerText = `Ohh no! The loss is ${loss} and the percent is${lossPercent}%`; 
         output.style.color = 'red';
     }else if(current>initial){
         var profit = (current-initial)*qty;
         var profitPercent = (profit/(initial*qty))*100;
-        output.innerText = `You are in profit ${profit} with ${profitPercent}%`;
+        output.innerText = `Wohh!! The profit is ${profit} and the percent is ${profitPercent}%`;
         output.style.color = 'green';
     }else{
         output.innerText ="No Pain No Gain , No Gain No Pain"
+        output.style.color = 'black'
     }
 }
 function clickHandler(){
